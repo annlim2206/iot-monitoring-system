2eb33e29221d00e469dd233bd8f92827a104f2ae
annlim <anyane0w@yandex.ru>
2026-07-27 22:21:14 +0300

    update design:)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from PIL import Image, ImageTk, ImageSequence
 import pyglet
-import tkinter.font as tkFont
+import tkinter.font as tkfont
 #Настройка окна
 root = Tk()
 root.title("Iot-monitoring")
@@ -12,10 +12,11 @@ icon = PhotoImage(file = "assets/icon.png")
 root.iconphoto(False, icon)
 pyglet.options['win32_gdi_font'] = True
 pyglet.font.add_file('assets/path/Akedoakushon-Regular.otf')
-default_font = tkFont.nametofont("TkDefaultFont")
+default_font = tkfont.nametofont("TkDefaultFont")
 default_font.configure(family="Akedoakushon-Regular", size=25)
+
 #Размещение Gif
-frames1, frames2, frames3, frames4 = [], [], [], []
+frames1, frames2, frames3, frames4, frames5 = [], [], [], [], []
 try:
     with Image.open("assets/cat.gif") as gif:
         for frame in ImageSequence.Iterator(gif):
@@ -33,16 +34,20 @@ try:
         for frame in ImageSequence.Iterator(gif):
             resized_frame = frame.convert('RGBA').resize((40, 40), Image.Resampling.LANCZOS)
             frames4.append(ImageTk.PhotoImage(resized_frame))
+    with Image.open("assets/steve.gif") as gif:
+        for frame in ImageSequence.Iterator(gif):
+            resized_frame = frame.convert('RGBA').resize((50, 50), Image.Resampling.LANCZOS)
+            frames5.append(ImageTk.PhotoImage(resized_frame))
 except FileNotFoundError:
     print("Файлы не найдены.")
 def update_gif(ind, frames, gif_label):
     if frames:
-        frame = frames[ind]
+        shot = frames[ind]
         ind += 1
         if ind == len(frames):
             ind = 0
-        gif_label.configure(image=frame)
-        root.after(250, update_gif, ind, frames, gif_label)
+        gif_label.configure(image=shot)
+        root.after(150, update_gif, ind, frames, gif_label)
 if frames1:
     gif_label1 = Label(root, bg='#f5badf')
     gif_label1.place(x=587, y=187)
@@ -52,14 +57,17 @@ if frames2:
     gif_label2.place(x=5, y=169)
     root.after(0, update_gif, 0, frames2, gif_label2)
 if frames3:
-    gif_label2 = Label(root, bg='#f5badf')
-    gif_label2.place(x=572, y=40)
-    root.after(0, update_gif, 0, frames3, gif_label2)
+    gif_label3 = Label(root, bg='#f5badf')
+    gif_label3.place(x=572, y=40)
+    root.after(0, update_gif, 0, frames3, gif_label3)
 if frames4:
-    gif_label2 = Label(root, bg='#f5badf')
-    gif_label2.place(x=5, y=10)
-    root.after(0, update_gif, 0, frames4, gif_label2)
-
+    gif_label4 = Label(root, bg='#f5badf')
+    gif_label4.place(x=5, y=10)
+    root.after(0, update_gif, 0, frames4, gif_label4)
+if frames5:
+    gif_label5 = Label(root, bg='#f5badf')
+    gif_label5.place(x=300, y=199)
+    root.after(0, update_gif, 0, frames5, gif_label5)
 #Текст
 label = Label(text="temperature", bg="#f5badf")
 label1 = Label(text="humidity", bg="#f5badf")
